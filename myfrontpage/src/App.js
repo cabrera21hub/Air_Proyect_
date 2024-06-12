df6468d89a8679f1c1920567f9145b00548996b0
Alan Cabrera <alanc9961693@gmail.com>
2024-06-12 11:33:09 -0600

    añadi la carpeta donde trabajaremos con reac

---
+++ myfrontpage/src/App.js
@@ -0,0 +1,12 @@
+// src/NuevoComponente.js
+import React from 'react';
+
+function NuevoComponente() {
+  return (
+    <div>
+      <h1>Hola, soy un nuevo componente!</h1>
+    </div>
+  );
+}
+
+export default NuevoComponente;
