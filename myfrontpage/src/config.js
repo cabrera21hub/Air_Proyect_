2831f9a4c621bf92581851dc3dbbf21a05931f6a
Alan Cabrera <alanc9961693@gmail.com>
2024-07-25 18:01:44 -0600

    cambios para verificar 2

--- myfrontpage/src/config.js
+++ myfrontpage/src/config.js
@@ -2,6 +2,5 @@
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 export const WEATHER_API_KEY = '702133c6def9d64b8a25243860be4e74';
 export const CITY_ID = '3530597'; // ID de la CDMX en OpenWeatherMap
-export const AIR_QUALITY_API_URL = 'http://api.airvisual.com/v2/city';//alan cabrea
-
+export const AIR_QUALITY_API_URL = 'http://api.airvisual.com/v2/city';
 export const AIR_QUALITY_API_KEY = 'c11fe070-6faa-4746-8140-c319fd960530'; // Tu clave API de IQAir
